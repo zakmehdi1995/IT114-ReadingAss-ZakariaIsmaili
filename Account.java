6dd254e899114ca5ec3f90dd0761b0ceffe7a0c0
Alt <Alt@10.200.158.29>
2016-11-02 16:30:02 -0400

    Final Commit

---
+++ Account.java
@@ -0,0 +1,12 @@
+package readingAss1_97_java_Ismaili;
+java.util.Date
+public class Account {
+
+	class Account() {
+		private int id;
+		private double balance;
+		private double annualInterestRate;
+		private Date dateCreated;
+	}
+
+}
